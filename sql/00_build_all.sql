ca0fb9831649284512b1eda0c6e8d0da975bf1ff
architkiran <architkiran@gmail.com>
2026-01-28 11:18:39 -0500

    Make KPI SQL compatible with sample dataset (remove non-essential joins)

---
+++ sql/00_build_all.sql
@@ -0,0 +1,6 @@
+.read sql/finance/finance_revenue_daily.sql
+.read sql/growth/growth_revenue_daily.sql
+.read sql/reconciliation/revenue_mismatch_daily.sql
+-- Optional if/when you have these:
+-- .read sql/canonical/canonical_revenue_daily.sql
+-- .read sql/reconciliation/compare_to_canonical.sql
